150ccc82246bcbfccd8927d376bc4a600f041030
kaamraan <kaamraank@outlook.com>
2017-01-20 18:18:12 +0530

    Add weather api key and JSON classes

--- UWPWeatherLite/MainPage.xaml.cs
+++ UWPWeatherLite/MainPage.xaml.cs
@@ -26,5 +26,9 @@ namespace UWPWeatherLite
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
